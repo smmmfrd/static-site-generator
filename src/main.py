ffbc66b84a5c72c3eda4501908033c51bdc556da
Sam Mumford <smmmfrd@gmail.com>
2025-12-05 17:44:47 -0800

    Build public directory with recursive function

--- src/main.py
+++ src/main.py
@@ -1,12 +1,31 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 
 def main():
-    test_node = TextNode(
-        "This is some anchor text", TextType.LINK_TEXT, "https://www.boot.dev"
-    )
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    print(test_node)
+    print("Copying static files to public dir...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+
+def copy_files_recursive(source_path, target_path):
+    if not os.path.exists(target_path):
+        os.mkdir(target_path)
+
+    for filename in os.listdir(source_path):
+        from_path = os.path.join(source_path, filename)
+        dest_path = os.path.join(target_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
 
 
 main()
